006d27d303ea128ac23e5f5ec9d455c0379b188a
Felipe-dot <felipeluis610@gmail.com>
2024-01-03 15:26:28 -0300

    Adding basic earth and sun interaction

--- main.js
+++ main.js
@@ -1,24 +1,99 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from "three";
+import Planet from "./pages/lib/Planet";
+import Rotation from "./pages/lib/Rotation";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-setupCounter(document.querySelector('#counter'))
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#bg"),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(15, 25, 35);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+
+scene.add(pointLight, ambientLight);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// const lightHelper = new THREE.PointLightHelper(pointLight);
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(lightHelper, gridHelper);/
+
+// function addStar() {
+//   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+//   const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+//   const star = new THREE.Mesh(geometry, material);
+
+//   const [x, y, z] = Array(3)
+//     .fill()
+//     .map(() => THREE.MathUtils.randFloatSpread(100));
+
+//   star.position.set(x, y, z);
+//   scene.add(star);
+// }
+
+// Array(200).fill().forEach(addStar);
+
+const spaceTexture = new THREE.TextureLoader().load("space.jpg");
+scene.background = spaceTexture;
+
+const sunGeometry = new THREE.SphereGeometry(8);
+const sunTexture = new THREE.TextureLoader().load("sun.jpeg");
+const sunMaterial = new THREE.MeshBasicMaterial({ map: sunTexture });
+const sunMesh = new THREE.Mesh(sunGeometry, sunMaterial);
+const solarSystem = new THREE.Group();
+solarSystem.add(sunMesh);
+
+scene.add(solarSystem);
+
+// const moonTexture = new THREE.TextureLoader().load("moon.jpg");
+// const normalTexture = new THREE.TextureLoader().load("normal.jpg");
+
+// const moon = new THREE.Mesh(
+//   new THREE.SphereGeometry(3, 32, 32),
+//   new THREE.MeshStandardMaterial({
+//     map: moonTexture,
+//     normalMap: normalTexture,
+//   })
+// );
+
+const earth = new Planet(4, 48, "earth.jpeg");
+const earthMesh = earth.getMesh();
+let earthSystem = new THREE.Group();
+earthSystem.add(earthMesh);
+
+solarSystem.add(earthSystem);
+
+const earthRotation = new Rotation(earthMesh);
+const earthRotationMesh = earthRotation.getMesh();
+earthSystem.add(earthRotationMesh);
+
+const EARTH_YEAR = 2 * Math.PI * (1 / 60) * (1 / 60);
+
+function animate() {
+  requestAnimationFrame(animate);
+  sunMesh.rotation.y += 0.001;
+  earthSystem.rotation.y += EARTH_YEAR;
+  controls.update();
+
+  renderer.render(scene, camera);
+}
+
+animate();
