b42ecd25f0484adb46c796dbbba136bb473facf5
Felipe-dot <felipeluis610@gmail.com>
2024-01-05 15:08:05 -0300

    Change create planet function

--- pages/lib/Planet.js
+++ pages/lib/Planet.js
@@ -1,20 +1,32 @@
 import * as THREE from "three";
 
-export default class Planet {
-  constructor(radius, positionX, textureFile) {
-    this.radius = radius;
-    this.positionX = positionX;
-    this.textureFile = textureFile;
-  }
+export default function createPlanet(size, texture, position, ring) {
+  const geometry = new THREE.SphereGeometry(size, 25, 20);
+  const material = new THREE.MeshStandardMaterial({
+    map: new THREE.TextureLoader().load(texture),
+  });
+  const planet = new THREE.Mesh(geometry, material);
+  const planetObj = new THREE.Object3D();
+  planetObj.add(planet);
+  planetObj.receiveShadow = true;
+  // scene.add(planetObj);
+  planet.position.x = position;
 
-  getMesh() {
-    if (this.mesh === undefined || this.mesh === null) {
-      const geometry = new THREE.SphereGeometry(this.radius);
-      const texture = new THREE.TextureLoader().load(this.textureFile);
-      const material = new THREE.MeshBasicMaterial({ map: texture });
-      this.mesh = new THREE.Mesh(geometry, material);
-      this.mesh.position.x += this.positionX;
-    }
-    return this.mesh;
+  if (ring) {
+    const RingGeo = new THREE.RingGeometry(
+      ring.innerRadius,
+      ring.outerRadius,
+      30
+    );
+    const RingMat = new THREE.MeshStandardMaterial({
+      map: new THREE.TextureLoader().load(ring.texture),
+      side: THREE.DoubleSide,
+    });
+    const Ring = new THREE.Mesh(RingGeo, RingMat);
+    planetObj.add(Ring);
+
+    Ring.position.x = position;
+    Ring.rotation.x = -0.5 * Math.PI;
   }
+  return { planet, planetObj };
 }
